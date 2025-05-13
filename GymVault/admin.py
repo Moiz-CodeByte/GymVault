dc269456c096cc5b190e6a88e81dfae779d2c948
MoizCodeByte <hello@abdulmoiz.net>
2025-05-13 20:01:35 +0500

    add models in admin for super admin

--- GymVault/admin.py
+++ GymVault/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import User, Gym, GymAdmin, MembershipPlan, Member, Payment, Locker
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Gym)
+admin.site.register(GymAdmin)
+admin.site.register(MembershipPlan)
+admin.site.register(Member)
+admin.site.register(Payment)
+admin.site.register(Locker)
