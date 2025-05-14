5a2f5003ba60d84a2cd47da917f4a86484050323
MoizCodeByte <hello@abdulmoiz.net>
2025-05-14 16:01:02 +0500

    add login and register and homepage of app

--- GymVault/views.py
+++ GymVault/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+# def home(request):
+#     return HttpResponse("Hello, World!")
 def home(request):
-    return HttpResponse("Hello, World!")
-def index(request):
-    return render(request, 'base.html')
+    return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
